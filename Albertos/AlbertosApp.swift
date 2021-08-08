ce3ef9fcb637551eaa962941f92193ccedb204b6
Chad Zeluff <czeluff@gmail.com>
2021-08-07 17:36:47 -0700

    Initial Commit

---
+++ Albertos/AlbertosApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlbertosApp.swift
+//  Albertos
+//
+//  Created by Chad Zeluff on 8/7/21.
+//
+
+import SwiftUI
+
+@main
+struct AlbertosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
